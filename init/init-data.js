4fa7f3813760f6d7e245f2e083a552e1ef7f8cef
guokewei <380121501@qq.com>
2018-05-06 09:39:12 +0800

    第一次创建

---
+++ init/init-data.js
@@ -0,0 +1,16 @@
+// 初始化数据库
+const getGoods = require('./getGoods')
+const getAdmin = require('./admin');
+
+;(async () => {
+	console.log("start");
+    await getGoods();
+    await getAdmin();
+    process.on('exit', (code) => {
+        console.log(`数据抓取完毕`);
+    });
+    process.exit()
+})()
+
+
+
